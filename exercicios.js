aa2c08972e551fab0ef9794f221f857e66abc8dd
RenandRosa <142436623+RenandRosa@users.noreply.github.com>
2023-08-22 17:01:18 -0300

    Create exercicios.js

---
+++ exercicios.js
@@ -0,0 +1,63 @@
+/*
+* Renan da Rosa
+*
+*/
+
+/*
+* 1. 10
+*    10 5
+* 2. 10 10 10
+* 3. let horasTrabalhadas = prompt("Quantas horas você trabalha por dia?")
+let recebeDia = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${horasTrabalhadas/recebeDia} por hora`)
+;.;;
+*/
+
+//Exercícios de escrita de código
+/*1.*/
+/*
+let nome ;
+let idade ;
+console.log(typeof nome);
+console.log(typeof idade);
+nome = prompt("Qual seu nome?");
+idade = prompt("Qual sua idade?");
+console.log(nome);
+console.log(idade);
+  Porque o prompt faz aparecer o comando na tela
+console.log("Olá", nome, "você tem", idade, "anos"); 
+*/
+
+/*2.*/
+/*
+let sim ;
+let já ;
+let não ;
+console.log(typeof sim);
+console.log(typeof já);
+console.log(typeof não);
+sim = prompt("Está usando roupas escuras?");
+já = prompt("Já bebeu agua hoje?");
+não = prompt("Já estudou para a prova?");
+console.log(sim);
+console.log(já);
+console.log(não);
+console.log("Está usando roupas escuras?", sim, "Já bebeu agua hoje?", já, "Já estudou para a prova?", não);
+*/
+
+/*3.*/
+/*
+let a = 10
+let b = 25
+let c ;
+c = a;
+a = b;
+b = c;
+console.log(a, b);
+*/
+
+/*Opcional*/
+const a = 10;
+const b = 5;
+const c = a + b;
+const d = a * b;
